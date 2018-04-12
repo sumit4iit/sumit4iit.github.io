2f3028187ce9bc10a9d5414bd93309d091522eac
Sumit Agrawal <sumitsa@amazon.com>
2018-04-12 06:14:50 +0530

    Revert "Adding amazon password tool pages"
    This reverts commit 5fcf2ffd95c27dde97fc728d0ff50465fa868245.

--- Amazon Password Tool_files/isItDown.js
+++
@@ -1,9 +0,0 @@
-// Is It Down? outage stripe for Password Service
-(function () {
-  var s = document.createElement('script');
-  s.type = 'text/javascript';
-  s.async = true;
-  s.src = "https://is-it-down.amazon.com/PasswordTool/components/5358/stripe.js"
-  var firstScript = document.getElementsByTagName('script')[0];
-  firstScript.parentNode.insertBefore(s, firstScript);
-})();
